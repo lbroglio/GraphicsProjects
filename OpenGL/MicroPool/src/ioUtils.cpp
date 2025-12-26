c38b02363a78bb4a547f0e4566c6a8b0a2bf782d
lbroglio <lukebroglio@gmail.com>
2025-12-26 09:20:05 -0600

    Makefile fixes

--- OpenGL/MicroPool/src/ioUtils.cpp
+++ OpenGL/MicroPool/src/ioUtils.cpp
@@ -1,26 +1,26 @@
 #include<vector>
 #include<string>
 #include<fstream>
+#include<iostream>
 
 #include "ioUtils.hpp"
 
 void writeBufferToPly(std::vector<float> buffer, std::string filePath){
-    std::fstream outFile(filePath);
-
+    std::ofstream outFile(filePath);
     if(!outFile.is_open()){
         throw new std::invalid_argument("The file " + filePath + " could not be opened for writing");
     }
 
     // Write the header
-    outFile << "ply";
-    outFile << "ascii 1.0";
-    outFile << "element vertex " << buffer.size() / 3;
-    outFile << "property float x";
-    outFile << "property float y";
-    outFile << "property float z";
+    outFile << "ply\n";
+    outFile << "ascii 1.0\n";
+    outFile << "element vertex " << buffer.size() / 3 << '\n';
+    outFile << "property float x\n";
+    outFile << "property float y\n";
+    outFile << "property float z\n";
 
     // Write the elements 
     for(int i = 0; i < buffer.size(); i+=3){
-        outFile << buffer[i] << " " << buffer[i+1] << " " << buffer[i+2];
+        outFile << buffer[i] << " " << buffer[i+1] << " " << buffer[i+2] << '\n';
     }
 }
